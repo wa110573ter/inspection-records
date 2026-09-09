dfff4c96c7a5b585ba89e2744ed020b8a806141a
wa110573ter <wa110573ter@gmail.com>
2026-09-10 00:15:05 +0800

    停用舊今日路線入口並回到查表工作台

--- app/routes/today/page.tsx
+++ app/routes/today/page.tsx
@@ -1,9 +1,9 @@
+import { redirect } from "next/navigation";
 import { requireChatGPTUser } from "../../chatgpt-auth";
-import SimplePinMap from "./simple-pin-map";
 
 export const dynamic = "force-dynamic";
 
 export default async function TodayRoutePage() {
   await requireChatGPTUser("/routes/today");
-  return <SimplePinMap />;
+  redirect("/?selectedMap=1");
 }
