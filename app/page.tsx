051684b902bf290d5e0283956e5063f16a6142e6
wa110573ter <wa110573ter@gmail.com>
2026-09-10 00:27:04 +0800

    Remove legacy route entry from home

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { requireChatGPTUser } from "./chatgpt-auth";
 import Workbench from "./workbench";
 
@@ -6,12 +5,5 @@ export const dynamic = "force-dynamic";
 
 export default async function Home() {
   const user = await requireChatGPTUser("/");
-  return (
-    <>
-      <Workbench userName={user.displayName} />
-      <Link href="/routes/today" hidden aria-hidden="true" tabIndex={-1}>
-        舊版今日路線
-      </Link>
-    </>
-  );
+  return <Workbench userName={user.displayName} />;
 }
